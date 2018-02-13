b500d1feabb1e4060097e037b23a78ad037400cf
andre <andre@LAPTOP-NIT3AHAN>
2018-02-13 11:54:03 +0100

    aggiunti i metodi getter e setter nel PannelloCerca

--- GesCon4_1/src/PannelloCerca.java
+++ GesCon4_1/src/PannelloCerca.java
@@ -3,5 +3,28 @@ import java.awt.event.*;
 import javax.swing.*;
 public class PannelloCerca extends JPanel
 {
+	private MotoreCongelatore motoreCongelatore;
+	private JFrame finestraSuperiore;
+	public PannelloCerca(MotoreCongelatore motoreCongelatore, JFrame finestraSuperiore)
+	{
+		
+	}
 	
+	//metodi getter e setter delle parametri passati al Pannello
+	public void setMotoreCongelatore(MotoreCongelatore motoreCongelatore)
+	{
+		this.motoreCongelatore = motoreCongelatore;
+	}
+	public MotoreCongelatore getMotoreCongelatore()
+	{
+		return this.motoreCongelatore;
+	}
+	public void setFinestraSuperiore(JFrame finestraSuperiore)
+	{
+		this.finestraSuperiore = finestraSuperiore;
+	}
+	public JFrame getFinestraSuperiore()
+	{
+		return this.finestraSuperiore;
+	}
 }
