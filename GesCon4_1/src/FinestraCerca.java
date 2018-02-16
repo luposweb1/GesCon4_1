aa650a396b23ed5d79aa578d226a20c3410166a0
mitic <mitic@DESKTOP-C0H1PUH>
2018-02-16 23:38:51 +0100

    vediamo un po se c'è conflitto

--- GesCon4_1/src/FinestraCerca.java
+++ GesCon4_1/src/FinestraCerca.java
@@ -30,6 +30,7 @@ public class FinestraCerca extends JFrame
 					getMotoreCongelatore().getPannelloDestraPannelloPrincipale().repaint();
 					//getMotoreCongelatore().getPannelloCentroPannelloPrincipale().repaint();
 					dispose();
+					//ciao ciup
 				}
 				if(i==1)
 				{
