cba8a1c498aa2ba1789a1f019d95fb17e1b02c53
mitic <mitic@DESKTOP-C0H1PUH>
2018-02-20 13:55:54 +0100

    iniziato a programmare i pulsanti di scelta nel pannello cerca

--- GesCon4_1/src/PannelloCerca.java
+++ GesCon4_1/src/PannelloCerca.java
@@ -22,11 +22,22 @@ public class PannelloCerca extends JPanel
 	private JRadioButton sceltaRicercaDataScadenza;
 	private JRadioButton sceltaRicercaDataInserimento;
 	private JRadioButton sceltaRicercaPerConfezioneAperta;
+	private JButton buttonInizioRicerca;
+	private JButton buttonNuovaRicerca;
+	
+	private boolean booleanSceltaRicercaPerNome = true; //impostato a true in quanto è la scelta di controllo che viene attivata subito
+	private boolean booleanSceltaRicercaDataScadenza;
+	private boolean booleanSceltaRicercaDataInserimento;
+	private boolean booleanSceltaRicercaPerConfezioneAperta;
 	
 	public PannelloCerca(MotoreCongelatore motoreCongelatore, JFrame finestraSuperiore)
 	{
 		this.setFinestraSuperiore(finestraSuperiore);
 		this.setMotoreCongelatore(motoreCongelatore);
+		this.setBooleanSceltaRicercaPerNome(true); //impostata a true perchè è la casella di controllo inizialmente attiva
+		this.setBooleanSceltaRicercaPerDataInserimento(false);
+		this.setBooleanSceltaRicercaPerDataScadenza(false);
+		this.setBooleanSceltaRicercaPerConfezioneAperta(false);
 		this.setLayout(new BorderLayout());
 		Border bordoPanel = BorderFactory.createRaisedSoftBevelBorder();
 		
@@ -79,6 +90,49 @@ public class PannelloCerca extends JPanel
 		return this.finestraSuperiore;
 	}
 	
+	/**
+	 * 
+	 * @author mitic
+	 * Metodo che serve per impostare i valori boolen delle caselle di controllo
+	 * 
+	 */
+	public void setBooleanSceltaRicercaPerNome(boolean scelta)
+	{
+		this.booleanSceltaRicercaPerNome = scelta;
+	}
+	public boolean getBooleanSceltaRicercaPerNome()
+	{
+		return this.booleanSceltaRicercaPerNome;
+	}
+	
+	public void setBooleanSceltaRicercaPerDataInserimento(boolean scelta)
+	{
+		this.booleanSceltaRicercaDataInserimento = scelta;
+	}
+	public boolean getBooleanSceltaRicercaPerDataInserimento()
+	{
+		return this.booleanSceltaRicercaDataInserimento;
+	}
+	
+	public void setBooleanSceltaRicercaPerDataScadenza(boolean scelta)
+	{
+		this.booleanSceltaRicercaDataScadenza = scelta;
+	}
+	public boolean getBooleanSceltaRicercaPerDataScadenza()
+	{
+		return this.booleanSceltaRicercaDataScadenza;
+	}
+	
+	public void setBooleanSceltaRicercaPerConfezioneAperta(boolean scelta)
+	{
+		this.booleanSceltaRicercaPerConfezioneAperta = scelta;
+	}
+	
+	public boolean getBooleanSceltaRicercaConfezioneAperta()
+	{
+		return this.booleanSceltaRicercaPerConfezioneAperta;
+	}
+	
 	//Pannelli
 	public class PannelloSopra extends JPanel
 	{
@@ -128,6 +182,89 @@ public class PannelloCerca extends JPanel
 			this.add(sceltaRicercaDataInserimento);
 			this.add(sceltaRicercaDataScadenza);
 			this.add(sceltaRicercaPerConfezioneAperta);
+			buttonInizioRicerca = new JButton("Ricerca");
+			this.add(buttonInizioRicerca);
+			buttonNuovaRicerca = new JButton("New Ric.");
+			this.add(new JLabel(""));
+			this.add(new JLabel(""));
+			this.add(new JLabel(""));
+			this.add(buttonNuovaRicerca);
+			
+			//test verifica variabili boolean
+			System.out.println("Nome " + getBooleanSceltaRicercaPerNome());
+			System.out.println("Ins " + getBooleanSceltaRicercaPerDataInserimento());
+			System.out.println("Sca " + getBooleanSceltaRicercaPerDataScadenza());
+			System.out.println("Conf " + getBooleanSceltaRicercaConfezioneAperta());
+			
+			
+			
+			//azioni
+			sceltaRicercaNome.addActionListener(new ActionListener()
+			{				
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					// TODO Auto-generated method stub
+					System.out.println("controlloCasellaTestPerNome");
+					
+					//imposto tutti i valori delle variabili boolean
+					setBooleanSceltaRicercaPerNome(true);
+					setBooleanSceltaRicercaPerDataInserimento(false);
+					setBooleanSceltaRicercaPerDataScadenza(false);
+					setBooleanSceltaRicercaPerConfezioneAperta(false);
+				}
+			});
+			
+			sceltaRicercaDataInserimento.addActionListener(new ActionListener()
+			{
+				
+				@Override
+				public void actionPerformed(ActionEvent e) 
+				{
+					// TODO Auto-generated method stub
+					System.out.println("casello controllo scelta ricerca per data inserimento");
+					setBooleanSceltaRicercaPerNome(false);
+					setBooleanSceltaRicercaPerDataInserimento(true);
+					setBooleanSceltaRicercaPerDataScadenza(false);
+					setBooleanSceltaRicercaPerConfezioneAperta(false);
+				}
+			});
+			
+			sceltaRicercaDataScadenza.addActionListener(new ActionListener() 
+			{
+				
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					// TODO Auto-generated method stub
+					System.out.println("casello controllo scelta ricerca per data scadenza");
+					setBooleanSceltaRicercaPerNome(false);
+					setBooleanSceltaRicercaPerDataInserimento(false);
+					setBooleanSceltaRicercaPerDataScadenza(true);
+					setBooleanSceltaRicercaPerConfezioneAperta(false);
+					
+				}
+			});
+			
+			sceltaRicercaPerConfezioneAperta.addActionListener(new ActionListener()
+			{
+				
+				@Override
+				public void actionPerformed(ActionEvent e) 
+				{
+					// TODO Auto-generated method stub
+					System.out.println("casello controllo scelta ricerca per confezione aperta");
+					setBooleanSceltaRicercaPerNome(false);
+					setBooleanSceltaRicercaPerDataInserimento(false);
+					setBooleanSceltaRicercaPerDataScadenza(false);
+					setBooleanSceltaRicercaPerConfezioneAperta(true);
+					
+				}
+			});
+			
+			
+			
 		}
 	}
+	
 }
